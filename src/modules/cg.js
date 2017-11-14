9235d7331e331a572dc3f98de94639e696d8d318
Gregory Assasie <gregjarvez@gmail.com>
2017-11-14 23:27:06 +0000

    mend
    reels view

--- src/modules/cg.js
+++ src/modules/cg.js
@@ -1,4 +1,4 @@
-var reels = `
+var reelStructure = `
  |--%s--|--%s--|--%s--|
  
  |--%s--|--%s--|--%s--|
@@ -6,11 +6,20 @@ var reels = `
  |--%s--|--%s--|--%s--|
 `;
 
-export function reels(context, state) {
-  return console.log.apply(context, [
-    reels,
-    ...state[0],
-    ...state[1],
-    ...state[2]
-  ]);
+var win = `you win %s`;
+
+export function reels(reelsArray) {
+  var each = 0;
+  var display = [reelStructure];
+  for (var i = 0; i < 9; i++) {
+    var j = i % reelsArray.length;
+    if (j === 3) each++;
+    display.push(reelsArray[j][each]);
+  }
+  return console.log.apply(null, display);
+}
+
+
+export function winMessage(message) {
+    return console.log(win, message);
 }
