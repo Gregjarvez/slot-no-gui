1efaca5773e2b23fdc6ea1deab52d6762b25269e
Gregory Assasie <gregjarvez@gmail.com>
2017-11-30 11:41:41 +0000

    winhandler test redo

---
+++ __test__/events.test.js
@@ -0,0 +1,8 @@
+import Events from '../src/modules/events/events.js';
+
+
+xdescribe('it should', () => {
+  test('return a single instance when called', () => {
+    const events = Events();
+  })
+})
--- __test__/winHandler.test.js
+++ __test__/winHandler.test.js
@@ -5,6 +5,20 @@ import makeGlobalDatabase from './database.js'
 const winHandlerDb = makeGlobalDatabase('winHandler')
 const actualsDb    = makeGlobalDatabase('actual')
 
+function modifier ({win, balance, payout}) {
+  return {
+    win,
+    accumulatedWin: payout,
+    balance,
+    payout,
+  }
+}
+
+function mockPayoutValue () {
+  return (3 * configuration.symbols[1].value *
+    configuration.coinValue)
+}
+
 describe('it should ', () => {
 
   beforeAll(() => {
@@ -25,6 +39,15 @@ describe('it should ', () => {
         win: false,
         accumulatedWin: 0,
         balance: 1000,
+        stake: 10,
+        payout: 0,
+        currency: 'GBP',
+      },
+      stateObjectAfterWin: {
+        grid: grid,
+        win: true,
+        accumulatedWin: 0,
+        balance: 990,
         stake: 10,
         payout: 0,
         currency: 'GBP',
@@ -106,14 +129,31 @@ describe('it should ', () => {
         const winStats = winLines.reduce(
           winHandler.assertWin(grid, Boolean), [])
 
-        const matchFound = winHandler.matchFound(winStats, grid)
-        const noMatchFound = winHandler.noMatchFound(grid);
+        const matchFound   = winHandler.matchFound(winStats, grid)
+        const noMatchFound = winHandler.noMatchFound(grid)
 
-        const stateObjectAfterWin = matchFound(stateObject)
-        const stateObjectAfterLose = noMatchFound(stateObject)
+        const mockPayout = mockPayoutValue()
 
-        expect(stateObjectAfterLose).not.toEqual(stateObject)
-        expect(stateObjectAfterWin).not.toEqual(stateObject)
+        const actualStateObjectAfterWin  = Object.assign(
+          {}, stateObject, modifier({
+            win: true,
+            balance: (stateObject.balance + mockPayout),
+            payout: mockPayout,
+          }),
+        )
+        const actualStateObjectAfterLose = Object.assign({}, stateObject,
+          modifier({
+            win: false,
+            balance: (stateObject.balance - stateObject.stake),
+            payout: 0,
+          }),
+        )
+
+        const expectedStateAfterLose = noMatchFound(stateObject)
+        const expectedStateAfterWin  = matchFound(stateObject)
+
+        expect(actualStateObjectAfterWin).not.toEqual(expectedStateAfterLose)
+        expect(actualStateObjectAfterLose).not.toEqual(expectedStateAfterWin)
       })
     })
   })
